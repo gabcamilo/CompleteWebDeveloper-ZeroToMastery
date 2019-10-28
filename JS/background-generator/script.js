5a695d970fd6aef63019cf4cde276fd1854e3f3e
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-28 16:26:45 -0300

    Improvement of random color generator

--- JS/background-generator/script.js
+++ JS/background-generator/script.js
@@ -6,12 +6,7 @@ var button = document.getElementById("random");
 
 
 function randomColor(){
-	var letters = '0123456789ABCDEF';
- 	var color = '#';
-	for (var i = 0; i < 6; i++) {
-    	color += letters[Math.floor(Math.random() * 16)];
-  	}
-  	return color;
+  	return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
 function setGradient() {
