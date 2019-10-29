3a94d5afa99db2390509652a4ce7106b2bc3621c
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-29 15:12:47 -0300

    Updates

---
+++ precache-manifest.05fbf586076678459185de7fb76388e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e79459c585d9a2141c594c1e7d982f",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e76c6e964a248367cecf",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/css/main.d10518c9.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "e76c6e964a248367cecf",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/js/main.14e37777.chunk.js"
+  },
+  {
+    "revision": "702664dd8d66a22dd9e3",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/js/runtime-main.dc6b1bd9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CompleteWebDeveloper-ZeroToMastery/React/robofriends/precache-manifest.75c868eee7dc42f569e186c01786d7d2.js"
+  "/CompleteWebDeveloper-ZeroToMastery/precache-manifest.05fbf586076678459185de7fb76388e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CompleteWebDeveloper-ZeroToMastery/React/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CompleteWebDeveloper-ZeroToMastery/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
