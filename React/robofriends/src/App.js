1f851efbe321ef6b22ce23f3e7d75d837acfc620
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-29 13:47:55 -0300

    roboFriends: data from web api

--- React/robofriends/src/App.js
+++ React/robofriends/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import "./App.css";
 class App extends Component {
@@ -14,7 +13,9 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		this.setState({robots: robots})
+		fetch('https://jsonplaceholder.typicode.com/users') //window methode to make requests to servers
+			.then(response=>response.json())
+			.then(users => this.setState({robots:users}));
 	}
 
 	onSearchChange = (event) => {
@@ -25,13 +26,19 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		});
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots} />
-			</div>
-		)
+
+		if(this.state.robots.length === 0 ){
+			return <h1>Loading...</h1>
+		}else{
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots} />
+				</div>
+			)
+		}
+		
 	}
 }
 
