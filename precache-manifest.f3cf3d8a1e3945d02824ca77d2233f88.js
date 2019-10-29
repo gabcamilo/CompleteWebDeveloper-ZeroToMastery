a9499f4095d1e8f2fb3325a04ef9b8833deecfb9
Gabriela Carvalho Camilo <gabriela.camilo@pca.com.br>
2019-10-29 15:06:56 -0300

    Updates

---
+++ precache-manifest.f3cf3d8a1e3945d02824ca77d2233f88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e2db88a7229e003e27999b9a12687a",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "69a54620867a2cabd1e3",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/css/main.cb9200ae.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "69a54620867a2cabd1e3",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/js/main.319dcf23.chunk.js"
+  },
+  {
+    "revision": "5d0f653ce7010af50cc5",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/js/runtime-main.196dceae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/CompleteWebDeveloper-ZeroToMastery/tree/master/React/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
